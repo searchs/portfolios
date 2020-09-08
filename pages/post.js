cea79b141a06950fd73f790828fe0907c1c58105
searchs <ola.ajibode@gmail.com>
2020-09-08 22:21:03 +0100

    build and deploy command in package.json

--- pages/post.js
+++ pages/post.js
@@ -5,7 +5,7 @@ import { withRouter } from 'next/router'
 
 const Post =  ({ router }) => (
   <Layout title={ router.query.title }>
-    <p>
+    <p style= {{ width: "80vw"}} >
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat a qui
       ipsa nam placeat corporis, quas maiores vero eius quisquam praesentium
       modi voluptas illum vitae aspernatur perferendis, dolore officia. Enim.
